9e7c98452b58e23542b0622183067bfbf734dd8d
Bradley Jackson <Bradj4017@outlook.com>
2021-04-11 18:33:21 -0400

    Added flyout and initial login

---
+++ SmartHeaterApp/SmartHeaterApp/MainNavigationPageFlyout.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHeaterApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainNavigationPageFlyout : ContentPage
+    {
+        public ListView ListView;
+
+        public MainNavigationPageFlyout()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainNavigationPageFlyoutViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainNavigationPageFlyoutViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MainNavigationPageFlyoutMenuItem> MenuItems { get; set; }
+
+            public MainNavigationPageFlyoutViewModel()
+            {
+                MenuItems = new ObservableCollection<MainNavigationPageFlyoutMenuItem>(new[]
+                {
+                    new MainNavigationPageFlyoutMenuItem { Id = 0, Title = "System Info" },
+                    new MainNavigationPageFlyoutMenuItem { Id = 1, Title = "Sheduled Heating" },
+                    new MainNavigationPageFlyoutMenuItem { Id = 2, Title = "Temperature Settings" },
+                    new MainNavigationPageFlyoutMenuItem { Id = 3, Title = "Logout" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ SmartHeaterApp/SmartHeaterApp/TemperatureSettings.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHeaterApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TemperatureSettings : ContentPage
+    {
+        Label temperatureScaleLabel = new Label();
+        public TemperatureSettings()
+        {
+            InitializeComponent();
+
+            temperatureScaleLabel.SetBinding(Label.TextProperty, new Binding("SelectedItem", source: TemperatureScalePicker));
+        }
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                temperatureScaleLabel.Text = (string)picker.ItemsSource[selectedIndex];
+            }
+        }
+
+        void SetTemperature(object sender, EventArgs e)
+        {
+            Console.WriteLine("submitted wifi creds");
+            try
+            {
+                //await Navigation.PushAsync(new NavigationPage(new SystemInfo()));
+                //if successful, notify user and direct to temperature setting page
+                //else tell user it was unsuccessful
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
