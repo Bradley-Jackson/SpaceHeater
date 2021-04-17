aafd5525aef115ffcf1642555cfb050be478978a
NourEddine Hijazi <nmhijazi15@gmail.com>
2021-04-17 09:19:56 -0400

    Basic http request

--- SmartHeaterApp/SmartHeaterApp/MainPage.xaml.cs
+++ SmartHeaterApp/SmartHeaterApp/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -32,6 +34,31 @@ namespace SmartHeaterApp
             else
             {
                 Console.WriteLine("Failed Login, try email: a, password: a");
+                WebRequest request = WebRequest.Create(
+              "http://65.189.242.145:8081/index.html");
+                // If required by the server, set the credentials.
+                //request.Credentials = CredentialCache.DefaultCredentials;
+
+                // Get the response.
+                WebResponse response = request.GetResponse();
+                // Display the status.
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+                // Get the stream containing content returned by the server.
+                // The using block ensures the stream is automatically closed.
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    string responseFromServer = reader.ReadToEnd();
+                    // Display the content.
+                    Console.WriteLine(responseFromServer);
+                }
+
+                // Close the response.
+                response.Close();
+
             }
         }
 
